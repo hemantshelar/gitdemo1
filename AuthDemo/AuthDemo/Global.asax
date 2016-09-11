2b725140e2d97e49b966ace8b4a3a6467517df63
hemantshelar <hemant.shelar@gmail.com>
2016-09-11 10:47:37 +1000

    Added MVC plugin.

---
+++ AuthDemo/AuthDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AuthDemo.Global" Language="C#" %>
---
+++ AuthDemo/AuthDemo/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AuthDemo
+{
+    public class Startup
+    {
+        //install-package microsoft.Owin  
+        public void Configuration(IAppBuilder app)
+        {
+            //inside Sytem.Web.Mvc
+            RouteTable.Routes.MapRoute("default", "{controller}/{action}/{id}", new { controller = "Home" , action = "Index",id = "0"});
+
+
+        }
+    }
+}
